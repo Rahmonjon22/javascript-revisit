6f729aab821e9aa70cb47d37552d44832dbe8264
Rahmonjon22 <rahmonjon2@yahoo.com>
2023-02-25 09:16:07 +0100

    start

---
+++ JavaScript-intro/index.js
@@ -0,0 +1,51 @@
+//  This how comments work
+//  var number  = 5; // in-line comment
+
+// ======== Data Types ========= ///
+/** Data Types:
+ * undefined, null,, boolean, string, symbol, number, and object
+
+undefined is not defined
+null is nothing
+boolean true false
+string is udner ""  , 
+symbol muteable , 
+number is 23423,
+object stores variables
+
+var myName = "John";
+myName = 8; // for whole program
+
+let ourName = "freecodeCamp"; // within the scope
+const pi = 3.14 // constant name 
+*/
+// ======== Storing Values with Assignment Operators ========= ///
+
+// var a;
+// var b = 2;
+// a = 7;
+// b= a;
+// console.log(a);
+// node index.js shows console.log(a)
+
+// ======== Initializing with Assignment Operators value ========= ///
+
+// var a = 9;
+// var b = 10;
+// var c = "I am a";
+// // Dont change code bellow
+// a = a + 1;
+// b = c + 1;
+// c = c + " String!";
+
+// ======== Declarations assignment ========= ///
+
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+
