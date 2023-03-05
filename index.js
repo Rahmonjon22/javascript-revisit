47b840e6775c733449031fb910a817896be30e29
Rahmonjon22 <rahmonjon2@yahoo.com>
2023-03-05 14:25:19 +0100

    switch statement

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 
 // ======== Data Types ========= ///
 
-
 /** Data Types:
  * undefined, null,, boolean, string, symbol, number, and object
 
@@ -21,7 +20,6 @@ myName = 8; // for whole program
 let ourName = "freecodeCamp"; // within the scope
 const pi = 3.14 // constant name 
 */
-
 
 // ======== Storing Values with Assignment Operators ========= ///
 
@@ -42,11 +40,7 @@ const pi = 3.14 // constant name
 // b = c + 1;
 // c = c + " String!";
 
-
-
 // ======== Declarations assignment ========= ///
-
-
 
 // var studlyCapVar;
 // var properCamelCase;
@@ -56,10 +50,7 @@ const pi = 3.14 // constant name
 // properCamelCase = "A String";
 // titleCaseOver = 9000;
 
-
 // ======== plus mines multiply assignment ========= ///
-
-
 
 // var a = 5;
 // var b = 12;
@@ -68,24 +59,19 @@ const pi = 3.14 // constant name
 
 // // only modify code bellow line
 
-//  a+=5  // ====> // a = a + 5; 
-//  b-=5  // ====> // b = b - 5; 
+//  a+=5  // ====> // a = a + 5;
+//  b-=5  // ====> // b = b - 5;
 //  c*=5  // ====> // c = c * 5;
-//  d/=5  // ====> // c = c * 5; 
+//  d/=5  // ====> // c = c * 5;
 
 // console.log(d);
 
-
 // ======== double quotes => Quote string ========= ///
-
-
 
 // var myStr = "I am a  \"double Quoted \" string inside  \"double quotes \" ";
 // console.log(myStr);
 
 // ======== single quotes => Quote string ========= ///
-
-
 
 // var myStr = '<a href="https://www.w3schools.com/">Visit W3Schools.com!</a>';
 
@@ -100,7 +86,7 @@ const pi = 3.14 // constant name
 \b backspace
 \f form feed
 */
- 
+
 // var myStr = "FirstLine\n\t\SecondLine\nThirdLine";
 // console.log(myStr);
 
@@ -126,10 +112,7 @@ const pi = 3.14 // constant name
 // myStr+= someAdjective;
 // console.log(myStr);
 
-
 // ========String length ========= ///
-
-
 
 // var firstNameLength = 0;
 // var firstName = "Ada";
@@ -141,10 +124,7 @@ const pi = 3.14 // constant name
 // lastNameLength= lastName.length;
 // console.log(lastNameLength);
 
-
 // ========Bracket notation ========= ///
-
-
 
 // var firstLetterOfFirstName = "";
 // var firstName = "Ada";
@@ -156,23 +136,15 @@ const pi = 3.14 // constant name
 // firstLetterOfLastName = lastName[0]
 // console.log(firstLetterOfLastName);
 
-
-
 // ======== String Immutability ========= ///
-
-
 
 // var myStr = "Jello World";
 
 // myStr[0] = "H"; // fix me, this way will not work
-// myStr = "Hello World"; 
+// myStr = "Hello World";
 // console.log(myStr);
 
-
-
 // ======== last letter in string ========= ///
-
-
 
 // var firstName = 'Abdurahmonjon';
 // var lastLetterOfFirstName = firstName[firstName.length -1];
@@ -185,48 +157,33 @@ const pi = 3.14 // constant name
 // var thirdLetterOfFirstName = firstName[firstName.length - 3];
 // console.log(thirdLetterOfFirstName);
 
-
-
-
 // ======== Word Blanks ========= ///
-
-
 
 // function wordBlanks(myNoun, myAdjective, myVerb, myAdverb){
 //     // your code bellow
 //     var result = "";
 //     result += "The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb
-//     // your code above 
+//     // your code above
 //     return result;
 // }
 // console.log(wordBlanks("dog", "big", "ran", "quickly"));
 // console.log(wordBlanks("bike", "slow", "flew", "slowly"));
 
-
-
 // ======== Arrays ========= ///
-
 
 // var ourArray = ["John", 23];
 // console.log(ourArray);
 // var myArray = ["Ibragimov", 31];
 // console.log(myArray);
 
-
-
 // ======== Nest Arrays ========= ///
-
-
 
 // var ourArray = [["the universe", 42], ["everything", 2023]];
 // console.log(ourArray);
 
 // var myArray = [ ["Bulls", 23], ["White Fox", 50]];
 
-
-
 // ======== access array data ========= ///
-
 
 // var ourArray = [50, 60, 70];
 // var ourData = ourArray[0];
@@ -236,13 +193,7 @@ const pi = 3.14 // constant name
 // var myData = myArray[0];
 // console.log(myData);
 
-
-
-
 // ======== change array  ========= ///
-
-
-
 
 // var ourArray = [50, 60, 70];
 // ourArray[1] = 45
@@ -252,18 +203,13 @@ const pi = 3.14 // constant name
 // myArray[myArray.length -1] = 20;
 // console.log(myArray);
 
-
-
-
 // ======== access multi-dimensional arrays  ========= ///
-
 
 // var myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [[10, 11, 12], 13, 12, 14]]
 // //  in here I am accessing to the 3thd array and second number 8 and changing it to 31
 // myArray[2][1] = 31;
 // var myData = myArray[2][1];
 // console.log(myData);
-
 
 // ========  Push array ========= ///
 
@@ -287,13 +233,10 @@ const pi = 3.14 // constant name
 // var removeFromOurArray = ourArray.pop()
 // console.log(removeFromOurArray); // => 3 output
 // console.log(ourArray); // => output [ 1, 2 ]
- 
+
 // var myArray = [["John", 31],["Amira", 03] ];
 // var removeFromMyArray = myArray.pop();
 // console.log(myArray); // output => [ ["John", 31] ]
-
-
-
 
 // ======== Shift array  ========= ///
 // Shift removes first item or array
@@ -301,7 +244,7 @@ const pi = 3.14 // constant name
 // var ourArray = [1, 2, 3];
 // var removeFromOurArray = ourArray.shift()
 // console.log(ourArray); // => output [ 2, 3 ]
- 
+
 // var myArray = [["John", 31],["Amira", 03] ];
 // var removeFromMyArray = myArray.shift();
 // console.log(myArray); // output => [ [ 'Amira', 3 ] ]
@@ -320,7 +263,6 @@ const pi = 3.14 // constant name
 // myArray.unshift(["Adolat", 27]); // output => [ [ 'Adolat', 27 ], [ 'Amira', 3 ] ]
 // console.log(myArray); // output => [ [ 'Amira', 3 ] ]
 
-
 // ========  Shopping list array of arrays ========= ///
 
 // var myList = [["cereal", 3],["juice", 4],["milk", 1],["bananas", 6],["apple", 5],["eggs", 10],["onion", 2] ];
@@ -329,8 +271,6 @@ const pi = 3.14 // constant name
 // myList.push(["nok", 5]); // output =>   [ 'nok', 5 ] in the end of array
 // myList.unshift(["anor", 3]);  // output => [ 'anor', 3 ], in the begging of the array
 // console.log(myList);
-
-
 
 // ======== Reusable Function  ========= ///
 
@@ -348,8 +288,6 @@ const pi = 3.14 // constant name
 // }
 // myReusableFunction(); // it is being called here
 
-
-
 // ======== Passing Values to Functions with Arguments  ========= ///
 
 // function ourFunctionWithArgs(a, b){
@@ -362,20 +300,19 @@ const pi = 3.14 // constant name
 // }
 // myFunctionWithArgs(5, 6);
 
-
 // ======== Global Scope & functions  ========= ///
 // variables outside of functions is global scope
 
 // var myGlobal = 10;
 // function fun1(){
-//     notGlobal = 2 // if var was used it would only show in fun1 not globally 
+//     notGlobal = 2 // if var was used it would only show in fun1 not globally
 //      // so when you declare without var it beccomes global evein inside function
 
 // }
 
 // function fun2(){
 //     var output = " ";
-   
+
 //     if (typeof myGlobal != "undefined"){
 //         output += "myGlobal: " + myGlobal;
 //     }
@@ -387,8 +324,6 @@ const pi = 3.14 // constant name
 // fun1();
 // fun2();
 
-
-
 // ======== Local Scope & function  ========= ///
 
 //  function myLocalScope(){
@@ -398,7 +333,6 @@ const pi = 3.14 // constant name
 //  myLocalScope();
 
 //  console.log(myVar); // output => myVar is not defined
-
 
 // ======== Global vs local Scope & function  ========= ///
 
@@ -412,8 +346,6 @@ const pi = 3.14 // constant name
 // console.log(outWear); // output => T-shirt
 // console.log(myOutFit()); // output => Jeans
 
-
-
 // ======== return value from function  ========= ///
 
 // function minusSeven(num){
@@ -426,9 +358,8 @@ const pi = 3.14 // constant name
 // }
 // console.log(timesfive(5));// output => 25
 
-
 // ========  Undefined value from function ========= ///
- 
+
 // var sum = 0;
 // function addThree(){
 //     sum = sum + 3
@@ -439,8 +370,6 @@ const pi = 3.14 // constant name
 //     sum += 5
 // }
 // console.log(addFive(2)); // output => undefined
-
-
 
 // ======== returned value   ========= ///
 
@@ -458,8 +387,6 @@ const pi = 3.14 // constant name
 // processed = processedArg(22);
 // console.log(processed);
 
-
-
 // ======== Stand in Line   ========= ///
 
 // function nextInLine(arr, item) {
@@ -471,12 +398,11 @@ const pi = 3.14 // constant name
 // var testArr = [1,2,3,4,5];
 
 // console.log("Before: ", JSON.stringify(testArr));
-// //  The JSON.stringify() static method converts a JavaScript value 
+// //  The JSON.stringify() static method converts a JavaScript value
 // //  to a JSON string, optionally replacing values if a replacer function is specified or
 // //  optionally including only the specified properties if a replacer array is specified.
 // console.log(nextInLine(testArr, 6));
 // console.log("After: " + JSON.stringify(testArr));
-
 
 // // ========  Booleans ========= ///
 
@@ -485,13 +411,10 @@ const pi = 3.14 // constant name
 // }
 // console.log(welcomeToBooleans());
 
-
-
-
 // ======== Conditional Logic with If statement ========= ///
 
 // function ourTrueOrFalse(isItTrue) {
-//     if(isItTrue ){ // this is variable 
+//     if(isItTrue ){ // this is variable
 //         return "yes, it is true";
 //     }
 //     return "no it is not true";
@@ -506,9 +429,6 @@ const pi = 3.14 // constant name
 // }
 // console.log(TrueOrFalse(true));
 
-
-
-
 // ========  EQUALITY  operators ========= ///
 
 // function testEqual(val) {
@@ -521,7 +441,6 @@ const pi = 3.14 // constant name
 // console.log(testEqual(10));
 
 // ======== Strict EQUALITY  operators  ========= ///
-
 
 // function testEqual(val) {
 //     if (val == 7) {
@@ -536,8 +455,6 @@ const pi = 3.14 // constant name
  * 3 === 3
  * 3 == '3' it does not convert in three equals but if it was two equals
  */
-
-
 
 // ======== Comapre different values  ========= ///
 
@@ -566,8 +483,7 @@ const pi = 3.14 // constant name
 //     return "Equal"
 // }
 // // Change this  Value to Test
-// console.log(testStrictNotEqual(10));  // output => not equal 
-
+// console.log(testStrictNotEqual(10));  // output => not equal
 
 // ======== Equality Operators  ========= ///
 
@@ -593,505 +509,298 @@ const pi = 3.14 // constant name
 // }
 // console.log(testGreaterThan(10));
 
-function testLessThan(val) {
-    if (val < 25) {
-        return "under 25";
-    }
-    if (val < 55) {
-        return "under 55";
-    }
-    return "55 or over";
-}
-console.log(testLessThan(10));
+// function testLessThan(val) {
+//     if (val < 25) {
+//         return "under 25";
+//     }
+//     if (val < 55) {
+//         return "under 55";
+//     }
+//     return "55 or over";
+// }
+// console.log(testLessThan(10));
+
+// function testLessorEqual(val) {
+//     if(val <= 12){
+//          return "Smaller than or equal to 12";
+//     }
+//    if (val <= 24 ) {
+//     return " smaller than or eqaul to 24";
+// }
+// return "more than 24";
+// }
+// console.log(testLessorEqual(10));
+
+// ========  nested if statement more then 2 ifs ========= ///
+
+// function testLogicalAnd(val) {
+//     if (val <= 50) {
+//         if (val >= 25) {
+//             return "yes";
+//         }
+//     }
+//     return "no";
+// }
+// and opertator in if statement
+// function testLogicalAnd(val) {
+//     if (val <= 50 && val >= 25) {
+//        return "yes"
+//     }
+//     return "no";
+// }
+// console.log(testLogicalAnd(30));
+// testLogicalAnd(10);
+
+// ======== comparision with logical or operator  ========= ///
+
+// function testLogicalOr(val) {
+//     if (val < 10) {
+//         return "Outside";
+//     }
+//     if (val > 20 ) {
+//        return "Outside";
+//     }
+//     return "Inside";
+// }
+// function testLogicalOr(val) {
+//     if (val < 10 || val > 20) {
+//         return "Outside";
+//     }
+//     return "Inside";
+// }
+// console.log(testLogicalOr(2));
+
+// ======== if else statement  ========= ///
+
+// function testElse(val) {
+//  var result = "";
+//  if (val > 5) {
+//     result = "bigger than 5";
+//  } else {
+//     result = "5 or smaller"
+//  }
+//  return result ;
+// }
+// console.log(testElse(2));
+
+// function testElseIf(val) {
+//   if (val > 10) {
+//     return "bigger than 10";
+//   } else if (val < 5) {
+//     return " smaller than 5";
+//   } else {
+//     return "between 5 and 10";
+//   }
+// }
+// console.log(testElseIf(8));
+
+// ======== order in else if statement  ========= ///
+// it is important to write your if else statement in order first comes smaller number so it can check
+
+// function orderMyLogic(val) {
+//   if (val < 5) {
+//     return "less than 5";
+//   } else if (val < 10) {
+//     return "less than 10";
+//   } else {
+//     return " greater than or equal to 10";
+//   }
+// }
+// console.log(orderMyLogic(7));
+
+// ======== chaining if else statements  ========= ///
+
+// function testSize(num) {
+//   if (num < 5) {
+//     return "Tiny";
+//   } else if (num < 10) {
+//     return "small";
+//   } else if (num < 15) {
+//     return "medium";
+//   } else if (num < 20) {
+//     return "large";
+//   } else {
+//     return "Huge";
+//   }
+// }
+// console.log(testSize(11));
+
+// ======== Golf Code  ========= ///
+
+// var names = [
+//   "Hole in one!",
+//   "Eagle",
+//   " Birdie ",
+//   " Par ",
+//   " Bogey ",
+//   " Double Bogey ",
+//   " Go Home! ",
+// ];
+// function golfScore(par, strokes) {
+//   if (strokes == 1) {
+//     return names[0];
+//   } else if (strokes <= par - 2) {
+//     return names[1];
+//   } else if (strokes == par - 1) {
+//     return names[2];
+//   } else if (strokes == par) {
+//     return names[3];
+//   } else if (strokes <= par + 1) {
+//     return names[4];
+//   } else if (strokes <= par + 2) {
+//     return names[5];
+//   } else if (strokes <= par + 3) {
+//     return names[6];
+//   } else {
+//     return "Change Me";
+//   }
+// }
+// console.log(golfScore(5, 8));
+/**
+ 1 => "Hole in one!",
+ <= par -2  => "Eagle", 
+ == par-1 => " Birdie ",
+ par => " Par ",
+ par + 1 => " Bogey ",
+ par + 2 => " Double Bogey ",
+ >= par + 3 => " Go Home! "
+ */
+
+// ========  Switch Statements ========= ///
+
+/*
+ write a switch statement which tests val and sets answer for the following conditions:
+1 - "Alpha"
+2 - "betta"
+3 - "gamma"
+4 - "delta"
+*/
+// function caseInSwitch(val) {
+//   var answer = "";
+//   // return answer;
+//   switch (val) {
+//     case 1:
+//       answer = "alpha";
+//       break;
+//     case 2:
+//       answer = "betta";
+//       break;
+//     case 3:
+//       answer = "gamma";
+//       break;
+//     case 4:
+//       answer = "delta";
+//       break;
+//   }
+//   return answer;
+// }
+// // caseInSwitch(1);
+// console.log(caseInSwitch(3));
+
+// Default options in switch statements
+
+// function switchOfStuff(val) {
+//   var answer = "";
+//   switch (val) {
+//     case "a":
+//       answer = "apple";
+//       break;
+//     case "b":
+//       answer = "banana";
+//       break;
+//     case "c":
+//       answer = "cucumber";
+//       break;
+//     default:
+//         answer = "Fruits";
+//       break;
+//   }
+//   return answer;
+// }
+// console.log(switchOfStuff("a"));
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
+// ========   ========= ///
 
 // ========   ========= ///
 
-
-
-
-
-
-
-
-
 // ========   ========= ///
 
-
-
-
-
-
-
-
-
 // ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
-
-// ========   ========= ///
-
-
-
-
-
-
-
-
