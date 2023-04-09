2d49b8889454cc662331a0c41d5edd5170ba8738
Rahmonjon22 <rahmonjon2@yahoo.com>
2023-04-09 19:21:12 +0200

    Prevent Object Mutation

--- index.js
+++ index.js
@@ -1372,11 +1372,31 @@ accessing object property with bracket notation
 // }
 // console.log(checkSign(0));
 
-// ======== difference var or let or const  ========= ///
+// ======== Prevent Object Mutation  ========= ///
+
+function freezeObj() {
+    "use strict";
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+    Object.freeze(MATH_CONSTANTS); // => when you dont want to change value then use Object.freeze
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch ( ex ) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+console.log(PI);
 
 
+// ======== Arrow Function  ========= ///
+// use arrow functions to write concise anonymous functions
+let magic = function(){
+    return new Date();
+}
 
-// ========   ========= ///
 
 // ========   ========= ///
 
